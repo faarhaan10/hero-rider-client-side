445cdf46a8012b704ad712d60640b7a6fd8b02bb
Farhan Elias <faarhaan10@gmail.com>
2022-01-01 03:53:30 +0600

    packages added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from './Pages/Home/Home/Home';
 import Login from "./Pages/Login/Login";
+import Packages from "./Pages/Packages/Packages";
 import Registration from "./Pages/Registration/Registration/Registration";
 import UserProfile from "./Pages/UserProfile/UserProfile";
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="login" element={<Login />} />
         <Route path="registration" element={<Registration />} />
         <Route path="profile" element={<UserProfile />} />
+        <Route path="packages" element={<Packages />} />
         {/* <Route path="teams" element={<Teams />}>
           <Route path=":teamId" element={<Team />} />
           <Route path="new" element={<NewTeamForm />} />
